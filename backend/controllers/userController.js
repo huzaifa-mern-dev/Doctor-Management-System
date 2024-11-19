43060f8ccdc96eef2b3c2272e1237d45e05ddea0
Muhammad Huzaifa <mhuzaifa3737@gmail.com>
2024-11-19 15:07:03 +0500

    user login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,53 +7,85 @@ import jwt from "jsonwebtoken";
 
 // api to register user
 
-
-
 const registerUser = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-        if (!name || !email || !password) {
-            return res.status(400).json({ success: false, message: "Please fill all the fields" });
-        }
-        if (password.length < 6) {
-            return res.status(400).json({ success: false, message: "Password should be of atleast 6 characters" });
-        }
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please fill all the fields" });
+    }
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password should be of atleast 6 characters",
+      });
+    }
 
-        if (!validator.isEmail(email)) {
-            return res.status(400).json({ success: false, message: "Please enter a valid email" });
-        }
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please enter a valid email" });
+    }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const userData = {
-        name,
-        email,
-        password: hashedPassword,
+      name,
+      email,
+      password: hashedPassword,
     };
 
     // save user to database
     const newUser = await userModel.create(userData);
     const user = await newUser.save();
 
-const token =  jwt.sign({ id: user._id }, process.env.JWT_SECRET )
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-res.json({success: true, token, message: "User registered successfully"});
+    res.json({ success: true, token, message: "User registered successfully" });
+  } catch (error) {
+    console.log(error); // Log the specific error
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while registering user",
+      error: error.message,
+    });
+  }
+};
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
 
-    } catch (error) {
-        console.log(error); // Log the specific error
-        res.status(500).json({
-            success: false,
-            message: "An error occurred while registering user",
-            error: error.message,
-        });
-        
-
-        
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
     }
-}
 
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({
+        success: true,
+        token,
+        message: "User logged in successfully",
+      });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error); // Log the specific error
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while login user",
+      error: error.message,
+    });
+  }
+};
 
-
-export { registerUser };
+export { registerUser, loginUser };
