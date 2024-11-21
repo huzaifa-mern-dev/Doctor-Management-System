6be8752d4aee001c34d02060b5937c0a6aea2d8d
Muhammad Huzaifa <mhuzaifa3737@gmail.com>
2024-11-21 16:25:26 +0500

    appointment model

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -14,7 +14,8 @@ const appointmentSchema = new mongoose.Schema({
   isCompleted: { type: Boolean, default: false },
 });
 
-
-const appointmentModel = mongoose.models.appointment || mongoose.model("appointment", appointmentSchema);
+const appointmentModel =
+  mongoose.models.appointment ||
+  mongoose.model("appointment", appointmentSchema);
 
 export default appointmentModel;
